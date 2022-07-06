a473a7c833ee8664e837e805ec197e72d10eac4f
sava <savamedvedev@gmail.com>
2022-07-06 23:55:00 +0800

    five-commit

--- service/user-service.js
+++ service/user-service.js
@@ -1,3 +1,18 @@
+const UserModel = require("../models/user-model");
+const bcrypt = require("bcrypt");
+
+
+
 class UserService {
-	
+  async registration(email, password) {
+    const candidate = await UserModel.findOne({ email });
+    if (candidate) {
+      throw new Error(
+        `Пользователь с почтовым адресом ${email} уже существует`
+      );
+    }
+    const user = await UserModel.create({ email, password });
+  }
 }
+
+module.exports = new UserService();
