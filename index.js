d4c126bd7a9b3e70cbef02707dd30decc1304dc3
sava <savamedvedev@gmail.com>
2022-07-06 23:36:38 +0800

    third-commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
+const router = require("./router/index");
 DB_URL =
   "mongodb+srv://admin:123@cluster0.ahxvvop.mongodb.net/auth_role?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
@@ -11,6 +12,12 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use("/api", router);
+
+
+
+
+
 
 const start = async () => {
   try {
