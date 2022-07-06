ca530bceaa266c0ee7c2b33f6184dc59936022ec
sava <savamedvedev@gmail.com>
2022-07-06 22:18:15 +0800

    first-commit

---
+++ index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+DB_URL =
+  "mongodb+srv://admin:123@cluster0.ahxvvop.mongodb.net/auth_role?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+const start = async () => {
+  try {
+    await mongoose
+      .connect(DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => console.log("Connected to database!!!"))
+      .catch((err) => console.log("Connected to ERROR!!!"));
+    app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
